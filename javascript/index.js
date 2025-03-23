471cab6fe45295581f5df3619d34ef76d6647609
xDaBix <21bmiit091@gmail.com>
2025-03-23 20:30:42 +0530

    23-03-25 2

--- javascript/index.js
+++ javascript/index.js
@@ -94,9 +94,21 @@ else{
 }
 
 
+var arr =[1,2,3,4,5,6,7,8,9]
 
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
 
+// arr.forEach(function(element){
+//     console.log(element)
+// })
 
-
+let j=0
+while (j<arr.length) {
+    console.log(arr[j])
+    j++
+    
+}
 
 
