c6511f58fc24d69ce41d401b602acfbc090c1de5
xDaBix <21bmiit091@gmail.com>
2025-03-29 15:55:44 +0530

    29-03-25

---
+++ pythonbasics/chapter8/01_functoins.py
@@ -0,0 +1,12 @@
+# a=12
+# b=45
+# c=56
+
+# avg=a+b+c/3
+# print(avg)
+
+def avg(a,b,c):
+    avg1=(a+b+c)/3
+    print(avg1)
+
+avg(3,3,3)
---
+++ pythonbasics/chapter8/02_quickquiz.py
@@ -0,0 +1,8 @@
+def goodday(name,ending):
+    print("Good morning " + name)
+    print(ending)
+    return "done"
+
+a=goodday("varun","shukriya")
+goodday("varun","shukriya")
+print(a)
