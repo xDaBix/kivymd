54523eaf06dc610b7f74c208bc743307ada242e9
xDaBix <21bmiit091@gmail.com>
2025-04-22 21:43:25 +0530

    22-04-25

---
+++ pythonbasics/chapter12/01_walrus.py
@@ -0,0 +1,4 @@
+if (n:= len([1,2,3,4,5,6]))>3:
+    print("list is long")
+else:
+    print("list is short")
---
+++ pythonbasics/chapter12/02_types.py
@@ -0,0 +1,7 @@
+n:int = 5
+name: str = "John"
+
+def sum(a: int, b: int) -> int:
+    return a + b
+
+print(sum(1, 2))
